2091801d4ad37ec7a73ac5cb65f6c3a62840951c
Tianyi Sun <tysun@bu.edu>
2019-03-28 18:10:43 -0400

    final_commit

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var fs = require("fs");
+/* GET external url  */
+// router.get('/get', function(req, res, next) {
+//   res.send(JSON.stringify({string: 'Hi This is Tianyi Sun API'}));
+//
+// });
+
+router.get('/', function(req, res, next) {
+
+
+  request({
+    url: 'https://random.dog/woof.json'
+  }, function (error, response, body) {
+    if (!error ) {
+      res.body = JSON.parse(body);
+      res.render( 'index',{title:'Dog', content:body});
+
+    }else{
+      throw new Error(error);
+    }
+
+
+
+  });
+  // {
+  //   uri: 'https://random.dog/woof.json'
+  //   // function(error, response, body) {
+  //   //   if (!error && response.statusCode === 200) {
+  //   //     console.log(body);
+  //   //     res.json(body);
+  //   //   }
+  //   // }
+  //   // request('http://google.com/doodle.png').pipe(fs.createWriteStream('doodle.png'));
+  //
+  // };
+
+  //res.send(JSON.stringify({string: 'Hi This is Tianyi Sun API'}));
+
+
+
+
+});
+
+module.exports = router;
